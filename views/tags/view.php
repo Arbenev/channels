0445ec49468127a0030226411f607a290d69fce4
Acround <acround@gmail.com>
2025-10-23 18:01:47 +0200

    Adding view for articles and videos 4

--- views/tags/view.php
+++ views/tags/view.php
@@ -14,72 +14,78 @@ $this->params['breadcrumbs'][] = $tag['name'] ?? '';
         <?= Html::a('Назад к тегам', ['index'], ['class' => 'btn btn-secondary']) ?>
     </p>
 
-    <?php if (isset($channelsProvider) && $channelsProvider->getCount()) : ?>
-        <h2>Каналы</h2>
-        <?= GridView::widget([
-            'dataProvider' => $channelsProvider,
-            'columns' => [
-                ['attribute' => 'id', 'label' => 'ID'],
-                [
-                    'attribute' => 'link',
-                    'format' => 'raw',
-                    'label' => 'Ссылка',
-                    'value' => function ($model) {
-                        return Html::a(Html::encode($model['link']), $model['link'], ['target' => '_blank']);
-                    }
+    <?php if (isset($channelsProvider)) : ?>
+        <h2>Каналы (<?= $channelsProvider->getTotalCount() ?>)</h2>
+        <?php if ($channelsProvider->getTotalCount() > 0): ?>
+            <?= GridView::widget([
+                'dataProvider' => $channelsProvider,
+                'columns' => [
+                    ['attribute' => 'id', 'label' => 'ID'],
+                    [
+                        'attribute' => 'link',
+                        'format' => 'raw',
+                        'label' => 'Ссылка',
+                        'value' => function ($model) {
+                            return Html::a(Html::encode($model['link']), $model['link'], ['target' => '_blank']);
+                        }
+                    ],
+                    'description:text:Описание',
                 ],
-                'description:text:Описание',
-            ],
-        ]) ?>
-    <?php else: ?>
-        <p>Нет каналов для этого тега.</p>
+            ]) ?>
+        <?php else: ?>
+            <p>Нет каналов для этого тега.</p>
+        <?php endif; ?>
     <?php endif; ?>
 
-    <?php if (isset($articlesProvider) && $articlesProvider->getCount() > 0) : ?>
-        <h2>Статьи</h2>
-        <?= GridView::widget([
-            'dataProvider' => $articlesProvider,
-            'columns' => [
-                ['attribute' => 'id', 'label' => 'ID'],
-                [
-                    'attribute' => 'url',
-                    'format' => 'raw',
-                    'label' => 'Ссылка',
-                    'value' => function ($model) {
-                        return Html::a(Html::encode($model['url']), $model['url'], ['target' => '_blank']);
-                    }
+    <?php if (isset($articlesProvider)) : ?>
+        <h2>Статьи (<?= $articlesProvider->getTotalCount() ?>)</h2>
+        <?php if ($articlesProvider->getTotalCount() > 0): ?>
+            <?= GridView::widget([
+                'dataProvider' => $articlesProvider,
+                'columns' => [
+                    ['attribute' => 'id', 'label' => 'ID'],
+                    [
+                        'attribute' => 'url',
+                        'format' => 'raw',
+                        'label' => 'Ссылка',
+                        'value' => function ($model) {
+                            return Html::a(Html::encode($model['url']), $model['url'], ['target' => '_blank']);
+                        }
+                    ],
+                    [
+                        'attribute' => 'title',
+                        'label' => 'Заголовок',
+                    ],
                 ],
-                [
-                    'attribute' => 'title',
-                    'label' => 'Заголовок',
-                ],
-            ],
-        ]) ?>
-    <?php else: ?>
-        <p>Нет статей для этого тега.</p>
+            ]) ?>
+        <?php else: ?>
+            <p>Нет статей для этого тега.</p>
+        <?php endif; ?>
     <?php endif; ?>
 
-    <?php if (isset($videosProvider) && $videosProvider->getCount() > 0) : ?>
-        <h2>Видео</h2>
-        <?= GridView::widget([
-            'dataProvider' => $videosProvider,
-            'columns' => [
-                ['attribute' => 'id', 'label' => 'ID'],
-                [
-                    'attribute' => 'url',
-                    'format' => 'raw',
-                    'label' => 'Ссылка',
-                    'value' => function ($model) {
-                        return Html::a(Html::encode($model['url']), $model['url'], ['target' => '_blank']);
-                    }
+    <?php if (isset($videosProvider)) : ?>
+        <h2>Видео (<?= $videosProvider->getTotalCount() ?>)</h2>
+        <?php if ($videosProvider->getTotalCount() > 0): ?>
+            <?= GridView::widget([
+                'dataProvider' => $videosProvider,
+                'columns' => [
+                    ['attribute' => 'id', 'label' => 'ID'],
+                    [
+                        'attribute' => 'url',
+                        'format' => 'raw',
+                        'label' => 'Ссылка',
+                        'value' => function ($model) {
+                            return Html::a(Html::encode($model['url']), $model['url'], ['target' => '_blank']);
+                        }
+                    ],
+                    [
+                        'attribute' => 'title',
+                        'label' => 'Заголовок',
+                    ],
                 ],
-                [
-                    'attribute' => 'title',
-                    'label' => 'Заголовок',
-                ],
-            ],
-        ]) ?>
-    <?php else: ?>
-        <p>Нет видео для этого тега.</p>
+            ]) ?>
+        <?php else: ?>
+            <p>Нет видео для этого тега.</p>
+        <?php endif; ?>
     <?php endif; ?>
 </div>
